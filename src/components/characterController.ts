dc3a2ed5dc390ed0cce38f907362a069701558bb
Ludovic Clolot <clolotludovic@gmail.com>
2025-02-17 19:23:30 +0100

    Issue #20: better jump and camera placements

--- src/components/characterController.ts
+++ src/components/characterController.ts
@@ -1,4 +1,4 @@
-import { AnimationGroup, Axis, Mesh, Vector3 } from "@babylonjs/core";
+import { AnimationGroup, Mesh, Ray, Vector3 } from "@babylonjs/core";
 import { LevelScene } from "../scenes/levelScene";
 import { InputManager } from "../inputManager";
 import { Component } from "./component";
@@ -8,69 +8,95 @@ export class CharacterController implements Component {
     private mesh: Mesh;
     private input: InputManager;
 
-    private linearSpeed: number = 0.1;
-    private jumpSpeed: number = 0.15;
-    private gravity: number = -0.06;
+    private linearSpeed: number = 20;
+    private gravity: number = -12;
+    private jumpSpeed: number = 20;
+    private jumpThreshold: number = 8;
+    private k: number = 3;
+
+
+    private jumpStartTime: number = 0;
+    private isJumping: boolean = false;
+    private remainingJumps: number = 3;
 
     private idleAnim: AnimationGroup;
+    private walkAnim: AnimationGroup;
     private runAnim: AnimationGroup;
+    private meshAnimations: Array<AnimationGroup> = [];
 
     constructor(mesh: Mesh, animations: Array<AnimationGroup>, input: InputManager, scene: LevelScene) {
         this.mesh = mesh;
         this.input = input;
         this.scene = scene;
         const idleAnim = animations.find(ag => ag.name.toLowerCase().includes("idle"));
+        const walkAnim = animations.find(ag => ag.name.toLowerCase().includes("walk"));
         const runAnim = animations.find(ag => ag.name.toLowerCase().includes("run"));
 
         if (!idleAnim) {
             throw new Error("Idle animation not found for " + mesh.name);
+        }
+        if (!walkAnim) {
+            throw new Error("Walk animation not found for " + mesh.name);
         }
         if (!runAnim) {
             throw new Error("Run animation not found for " + mesh.name);
         }
 
         this.idleAnim = idleAnim;
+        this.walkAnim = walkAnim;
         this.runAnim = runAnim;
-        this.playIdleAnim();
+        this.meshAnimations.push(this.idleAnim);
+        this.meshAnimations.push(this.walkAnim);
+        this.meshAnimations.push(this.runAnim);
+        this.playAnimation(this.idleAnim);
+    }
+
+    private playAnimation(anim: AnimationGroup): void {
+        if (!anim.isPlaying) {
+            this.meshAnimations.forEach(ag => ag.stop());
+            anim.play(true);
+        }
     }
 
     public beforeRenderUpdate(): void {
-        this.updateFromControls();
-    }
+        const deltaTime = this.scene.getEngine().getDeltaTime() / 1000;
 
-    private playIdleAnim(): void {
-        if (!this.idleAnim.isPlaying) {
-            this.runAnim.stop();
-            this.idleAnim.play(true);
+        if (this.input.inputMap[this.input.jumpKey] && !this.isJumping && this.remainingJumps) {
+            this.jumpStartTime = performance.now();
+            this.isJumping = true;
+            this.remainingJumps--;
         }
-    }
 
-    private playRunAnim(): void {
-        if (!this.runAnim.isPlaying) {
-            this.idleAnim.stop();
-            this.runAnim.play(true);
+        if (this.isJumping && this.jumpStartTime) {
+            const elapsedTime = (performance.now() - this.jumpStartTime) / 1000;
+            const jumpVelocity = this.jumpSpeed * Math.exp(-this.k * elapsedTime);
+
+            if (jumpVelocity > this.jumpThreshold)
+                this.mesh.moveWithCollisions(new Vector3(0, jumpVelocity * deltaTime, 0));
+            else
+                this.isJumping = false;
         }
-    }
-
-    private updateFromControls(): void {
-        // const deltaTime = this.scene.getEngine().getDeltaTime() * this.deltaTimeFactor;
-        this.mesh.moveWithCollisions(new Vector3(0, this.gravity, 0));
-
-        if (this.input.inputMap[this.input.jumpKey] && this.mesh.position.y < 15) {
-            this.mesh.moveWithCollisions(Axis.Y.scale(this.jumpSpeed));
-        }
+        else
+            this.mesh.moveWithCollisions(new Vector3(0, this.gravity * deltaTime, 0));
 
         if (this.input.inputMap[this.input.rightKey]) {
             this.mesh.rotation = new Vector3(0, Math.PI / 2, 0);
-            this.playRunAnim();
-            this.mesh.moveWithCollisions(this.mesh.forward.scale(this.linearSpeed));
+            this.playAnimation(this.runAnim);
+            this.mesh.moveWithCollisions(this.mesh.forward.scale(this.linearSpeed * deltaTime));
         }
         else if (this.input.inputMap[this.input.leftKey]) {
             this.mesh.rotation = new Vector3(0, -Math.PI / 2, 0);
-            this.playRunAnim();
-            this.mesh.moveWithCollisions(this.mesh.forward.scale(this.linearSpeed));
+            this.playAnimation(this.runAnim);
+            this.mesh.moveWithCollisions(this.mesh.forward.scale(this.linearSpeed * deltaTime));
         }
         else
-            this.playIdleAnim();
+            this.playAnimation(this.idleAnim);
+
+        // detect if grounded
+        const ray = new Ray(new Vector3(this.mesh.position.x, this.mesh.position.y - 1, this.mesh.position.z), Vector3.Down(), 1);
+        const hit = this.scene.pickWithRay(ray);
+
+        if (hit && hit.pickedMesh && hit.pickedMesh != this.mesh)
+            this.remainingJumps = 3;
     }
 }
